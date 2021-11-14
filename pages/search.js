77776fdad8a1bfbdb99922a3a7b9e391de9bc433
anisayed <ayed.aniss@hotmail.com>
2021-11-14 19:38:20 +0100

    Feat: search locations

--- pages/search.js
+++ pages/search.js
@@ -19,7 +19,12 @@ const Search = () => {
                     <Input placeholder="Enter pickup location"/>
                     <Input placeholder="Where to?"/>
                 </InputBoxes>
+                <PlusIcon src="https://img.icons8.com/ios/50/000000/plus-math.png"/>
             </InputContainer>
+            <SavedPlaces>
+                <StarIcon src="https://img.icons8.com/ios-filled/50/ffffff/star--v1.png"/>
+                Saved Places
+            </SavedPlaces>
         </Wrapper>
     )
 }
@@ -28,24 +33,38 @@ const Wrapper = tw.div`
     bg-gray-200 h-screen
 `
 const ButtonContainer = tw.div`
-    bg-white rounded-full px-4
+    bg-white px-4
 `
 const BuckButton = tw.img`
     h-12
 `
 const InputContainer = tw.div`
-    flex items-center bg-white
+    flex items-center bg-white px-4 mb-2
 `
 const FromToIcons = tw.div`
-    w-10 flex flex-col
+    w-10 flex flex-col mr-2 items-center
 `
-const Circle = tw.img``
-const Line = tw.img``
-const Square = tw.img``
+const Circle = tw.img`
+    h-2.5
+`
+const Line = tw.img`
+    h-10
+`
+const Square = tw.img`
+    h-3
+`
 const InputBoxes = tw.div`
     flex flex-col flex-1
 `
 const Input = tw.input`
-    h-10 bg-gray-200 my-2 rounded-2 p-2 outline-none border-none
+    h-10 bg-gray-200 my-2 rounded-2 p-2 outline-none border-none 
 `
-
+const PlusIcon = tw.img`
+    w-10 h-10 bg-gray-200 rounded-full ml-3 p-1
+`
+const SavedPlaces = tw.div`
+    flex items-center bg-white px-4 py-2
+`
+const StarIcon = tw.img`
+    bg-gray-400 w-10 h-10 p-2 mr-2 rounded-full
+`
